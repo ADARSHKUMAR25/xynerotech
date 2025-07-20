c204c1cc3e1f323cf4d8f6699529f3666e6367ad
ADARSHKUMAR25 <AADINOKET@GMAIL.COM>
2025-07-21 00:30:51 +0530

    first commit

---
+++ xynerotech/script.js
@@ -0,0 +1,10 @@
+import Lenis from 'https://cdn.skypack.dev/@studio-freight/lenis'
+
+const lenis = new Lenis()
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
